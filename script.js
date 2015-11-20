b8ff0d0aaf20fe1cd27b303ba074f08fbb07399b
Zachary Schweikert <zachary.schweikert@west-mec.com>
2015-11-20 15:23:14 -0700

    It drops down onto the page after waiting a couple of seconds.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-    settimeout(myfunction, 1000);
-    function myfunction (){ $('#clientsOpen').click(function() {
+    setTimeout(function(){
         $('#clientsDropDown #clientsDashboard').slideToggle(300);
         $(this).toggleClass('clientsClose');
-    }); // end click}
+    }, 2000);
 
-}
+
 
 });
