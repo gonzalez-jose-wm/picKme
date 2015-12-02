c85c082487c0908da14aebd4240a9131127e38ef
Zachary Schweikert <zachary.schweikert@west-mec.com>
2015-12-02 14:06:39 -0700

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+
     setTimeout(function(){
-        $('#clientsDashboard').slideToggle(800);
+        $('.clientsCTA').slideToggle(800);
     }, 2000);
 
 
@@ -9,5 +10,29 @@ $(document).ready(function() {
 });
 
 function submit () {
-    alert("HI")
+
+    username = $('#username').val();
+    password = $('#password').val();
+
+    if (username.length > 0 && password.length > 0) {
+        loggedIn = 1;
+        $('.clientsCTA').slideToggle(800)
+    }
+    if (username.length > 0 && password.length == 0){
+        $('#error').html("Password field can not be left blank.")
+
+    }
+    if (username.length == 0 && password.length > 0) {
+        $('#error').html("Username field can not be left blank.")
+    }
+    if (username.length == 0 && password.length == 0) {
+        $('#error').html("Username and password fields are required.")
+    }
+
+
+
+
+
+
+
 }
