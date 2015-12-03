186283f1fd548beaf2e09c9fe706c98449597e5e
Zachary Schweikert <zachary.schweikert@west-mec.com>
2015-12-03 14:30:41 -0700

    Pop up login is now over the categories page.

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
 
     setTimeout(function(){
-        $('.clientsCTA').slideToggle(800);
+        $('#clientsCTA').slideToggle(800);
     }, 2000);
-
-
 
 
 });
 
-function submit () {
+function signin () {
 
     username = $('#username').val();
     password = $('#password').val();
 
     if (username.length > 0 && password.length > 0) {
         loggedIn = 1;
-        $('.clientsCTA').slideToggle(800)
+        $('#clientsCTA').slideToggle(800)
     }
     if (username.length > 0 && password.length == 0){
         $('#error').html("Password field can not be left blank.")
@@ -28,10 +26,17 @@ function submit () {
     if (username.length == 0 && password.length == 0) {
         $('#error').html("Username and password fields are required.")
     }
-
-
-
-
+}
+function signup () {
+    $('#clientsCTA').html("
+        New Username:
+        <input type='text' id='username'><br>
+        New Password:
+<input type='text' id='password'><br>
+        <button onClick='signin()'>Submit</button>
+        <button onClick='signup()'>Sign Up</button>
+    <br>
+    <div id='error'></div>");
 
 
 
