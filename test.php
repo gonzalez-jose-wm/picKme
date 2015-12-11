800630ac09881fac8ce2d7caaefb0f4305d954d2
Zachary Schweikert <zachary.schweikert@west-mec.com>
2015-12-11 14:59:37 -0700

    Just a test to start working on the php. Just ignore this file but please do not delete it.

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session1
+ * Date: 12/11/15
+ * Time: 2:54 PM
+ */
+
+echo "this is a test";
+
+/** search something like "vagrant install" to find out how to do the install */
+    /** or search something like "connect vagrant to ubuntu/virtualbox"  */
+    /** this was a website that helped Brandon in the file to allow permissions to enter the server. */
+
+/** to start vagrant server enter "vagrant up" into the console. */
+/** to end the server enter "vagrant halt" into the console. */
+/** to restart the server completely enter "vagrant refresh" into the console. */
