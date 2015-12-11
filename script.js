dfe0e8848667206a8e6ca1bbbdb22ea8dfc74d4a
Zachary Schweikert <zachary.schweikert@west-mec.com>
2015-12-11 12:58:49 -0700

    Zach's finished part.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-<<<<<<< HEAD
+
     setTimeout(function(){
         $('#grayBack').slideToggle(800);
 
@@ -13,26 +13,45 @@ $(document).ready(function() {
 });
 
 function signin () {
-=======
+    username = $('#username').val();
+    password = $('#password').val();
 
-    setTimeout(function(){
-        $('.clientsCTA').slideToggle(800);
-    }, 2000);
+    if (username.length > 0 && password.length == 0){
+        email2 =document.getElementById("password");
+        email2.style.backgroundColor = "palegoldenrod";
+        email3 =document.getElementById("username");
+        email3.style.backgroundColor = "";
+        $('#error').html("Password field can not be left blank.")
+    }
+    if (username.length == 0 && password.length > 0) {
+        email2 =document.getElementById("username");
+        email2.style.backgroundColor = "palegoldenrod";
+        email3 =document.getElementById("password");
+        email3.style.backgroundColor = "";
+        $('#error').html("Username field can not be left blank.")
+    }
+    if (username.length == 0 && password.length == 0) {
+        email2 =document.getElementById("password");
+        email2.style.backgroundColor = "palegoldenrod";
+        email3 =document.getElementById("username");
+        email3.style.backgroundColor = "palegoldenrod";
+        $('#error').html("Username and password fields are required.")
+    }
+    if (username.length > 0 && password.length > 0) {
+        $('#grayBack').slideToggle(800)
+    }
 
+}
 
-
-
-});
 
 function submit () {
->>>>>>> 7675a64073dcc4ca854651761fe128504941f66a
 
     username = $('#username').val();
     password = $('#password').val();
 
     if (username.length > 0 && password.length > 0) {
         loggedIn = 1;
-<<<<<<< HEAD
+
         $('#grayBack').slideToggle(800)
     }
     if (username.length > 0 && password.length == 0){
@@ -138,25 +157,12 @@ function slideshow() {
         setInterval(slideshow, 2000);
         running = 1;
     }
-=======
-        $('.clientsCTA').slideToggle(800)
-    }
-    if (username.length > 0 && password.length == 0){
-        $('#error').html("Password field can not be left blank.")
-
-    }
-    if (username.length == 0 && password.length > 0) {
-        $('#error').html("Username field can not be left blank.")
-    }
-    if (username.length == 0 && password.length == 0) {
-        $('#error').html("Username and password fields are required.")
-    }
 
 
 
 
 
 
->>>>>>> 7675a64073dcc4ca854651761fe128504941f66a
+
 
 }
